c26266b6006f58bdbff1517d499e85c16c8d28e6
Lam Tan <lamtan1411@gmail.com>
2026-05-22 17:34:43 +0700

    feat: add base page

---
+++ pom/page/base.page.ts
@@ -0,0 +1,15 @@
+import { Page, Locator } from "@playwright/test";
+import { HeaderComponent } from "../components/header.component";
+import { FooterComponent } from "../components/footer.component";
+
+export class BasePage {
+  readonly page: Page;
+  readonly header: HeaderComponent;
+  readonly footer: FooterComponent;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.header = new HeaderComponent(page);
+    this.footer = new FooterComponent(page);
+  }
+}
